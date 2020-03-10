4acc02a92e34171971e40da2ebaa1d1a947b8e32
Dieudonné Willems <don_willems@me.com>
2020-03-10 23:29:27 +0100

    Initial XCode project setup

---
+++ SwiftINDI/SwiftINDI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftINDI.h
+//  SwiftINDI
+//
+//  Created by Don Willems on 10/03/2020.
+//  Copyright © 2020 lapsedpacifist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftINDI.
+FOUNDATION_EXPORT double SwiftINDIVersionNumber;
+
+//! Project version string for SwiftINDI.
+FOUNDATION_EXPORT const unsigned char SwiftINDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftINDI/PublicHeader.h>
+
+

d0518b5737643c2d9d52d525c794cb7966ea9482
Dieudonné Willems <don_willems@me.com>
2020-03-12 12:48:41 +0100

    Moved to SwiftSocket

--- SwiftINDITests/SwiftINDITests.swift
+++ SwiftINDITests/SwiftINDITests.swift
@@ -20,8 +20,7 @@ class SwiftINDITests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let client = BasicINDIClient(server: "revisionist.local", port: 7642)
     }
 
     func testPerformanceExample() {
